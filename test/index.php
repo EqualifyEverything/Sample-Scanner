7df6890f7e071786b6f85b7e7c48b6c18ee784bb
Blake Bertuccelli <blakebertuccelli@gmail.com>
2022-11-23 10:58:00 -0500

    Add Node

---
+++ test/index.php
@@ -0,0 +1,25 @@
+
+<?php
+$curl = curl_init($_GET['url']);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTP | CURLPROTO_HTTPS);
+curl_setopt($curl, CURLOPT_REDIR_PROTOCOLS, CURLPROTO_HTTP | CURLPROTO_HTTPS);
+curl_setopt($curl, CURLOPT_USERAGENT, 'Axe.Equalify');
+curl_exec($curl);
+curl_close($curl);
+?>
+
+<script src="axe.min.js"></script>
+<script>
+axe.run(document, function (err, results) {
+    if (err) throw err;
+    document.querySelector('html').remove();
+    const violations = JSON.stringify(results.violations);
+    const violations_escaped = violations.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
+    document.removeChild(document.documentElement);
+    document.write(violations_escaped);
+});
+</script>
