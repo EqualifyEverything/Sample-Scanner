5b7382ec513084bfb6f33ffe0ff1aab4d4796ac2
Blake Bertuccelli-Booth <blakebertuccelli@gmail.com>
2022-11-30 15:39:36 -0500

    Update header and output

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 // Report Errors
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 
-// Axe Web Service
+// Add JSON header
+header('Content-Type: application/json;');
+
+// axe-core Web Service
 $url = $_GET['url'];
 exec('axe '.$url.' --chromedriver-path /usr/local/bin/chromedriver --chrome-options="no-sandbox" --stdout', $output, $retval);
 foreach($output as $item){
-  print_r(htmlspecialchars($item));
+  echo $item;
 }
 ?>
