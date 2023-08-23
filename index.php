d9909d59f94b5f957d066b5ee2b2b1e9799a3a5a
Viktor Szépe <viktor@szepe.net>
2023-08-23 04:58:06 +0200

    Clean up index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
+
 // Report Errors
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 // Add JSON header
 header('Content-Type: application/json;');
 
 // axe-core Web Service
 $url = $_GET['url'];
-exec('axe '.$url.' --chromedriver-path /usr/local/bin/chromedriver --chrome-options="no-sandbox" --stdout', $output, $retval);
-foreach($output as $item){
-  echo $item;
-}
-?>
+exec('axe '.$url.' --chromedriver-path /usr/local/bin/chromedriver --chrome-options no-sandbox --stdout', $output);
+
+echo implode('', $output);
